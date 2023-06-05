31381befea5c94b74e9111f71e5b6d2bcbb5091a
onifernando1 <onifernando1@gmail.com>
2023-06-05 23:00:50 +0100

    add localStrategy passport code in appjs

--- askfm/app.js
+++ askfm/app.js
@@ -56,6 +56,36 @@ mongoose.connect(mongoDb, {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
