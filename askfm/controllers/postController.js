b7707af117e919e75bef2d2b7fff568d3102b442
onifernando1 <onifernando1@gmail.com>
2023-06-07 16:46:46 +0100

    move post new from routes to controller

--- askfm/controllers/postController.js
+++ askfm/controllers/postController.js
@@ -7,32 +7,37 @@ exports.post_list = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Post list");
 });
 
-//Display specific post
-exports.post_detail = asyncHandler(async(req, res, next)=.{
-    res.send(`NOT IMPLEMENTED: Post detail: ${req.params.id}`);
+// //Display specific post
+// exports.post_detail = asyncHandler(async(req, res, next)={
+//     res.send(`NOT IMPLEMENTED: Post detail: ${req.params.id}`);
+// });
+
+//Handle Post create form GET
+exports.post_create_get = asyncHandler(async (req, res, next) => {
+  res.render("new-post-form", { user: res.locals.currentUser });
 });
 
-//Handle Post create 
-exports.post_create_post = asyncHandler( async(req,res,next)=>{
-    res.send("Not implemented: Post create POST")
-})
+//Handle Post create
+exports.post_create_post = asyncHandler(async (req, res, next) => {
+  res.send("Not implemented: Post create POST");
+});
 
-//Handle Post delete form  
-exports.post_delete_get = asyncHandler( async(req,res,next)=>{
-    res.send("Not implemented: Post delete GET")
-})
+//Handle Post delete form
+exports.post_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("Not implemented: Post delete GET");
+});
 
-//Handle Post delete 
-exports.post_delete_post = asyncHandler( async(req,res,next)=>{
-    res.send("Not implemented: Post delete POST")
-})
+//Handle Post delete
+exports.post_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("Not implemented: Post delete POST");
+});
 
 //Handle Post update form GET
-exports.post_update_get = asyncHandler( async(req,res,next)=>{
-    res.send("Not implemented: Post update GET")
-})
+exports.post_update_get = asyncHandler(async (req, res, next) => {
+  res.send("Not implemented: Post update GET");
+});
 
 //Handle post update POST
-exports.post_update_post = asyncHandler( async(req,res,next)=>{
-    res.send("Not implemented: Post update POST")
-})
+exports.post_update_post = asyncHandler(async (req, res, next) => {
+  res.send("Not implemented: Post update POST");
+});
--- askfm/routes/posts.js
+++ askfm/routes/posts.js
@@ -3,10 +3,13 @@ var router = express.Router();
 const User = require("../models/userModel");
 const Post = require("../models/postModel");
 const mongoose = require("mongoose");
+const post_controller = require("../controllers/postController");
 
-router.get("/new", function (req, res, next) {
-  res.render("new-post-form", { user: res.locals.currentUser });
-});
+// router.get("/new", function (req, res, next) {
+//   res.render("new-post-form", { user: res.locals.currentUser });
+// });
+
+router.get("/new", post_controller.post_create_get);
 
 router.post("/new", async function (req, res, next) {
   try {
